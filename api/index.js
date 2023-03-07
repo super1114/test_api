110e2a4b4fbf4613ee55eb93c79d45b0afcaa90e
super1114 <super1114dev@gmail.com>
2023-03-07 11:22:14 +0900

    fix index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const app = require('express')();
 
 app.get('/api', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     res.send("Hello Truecolor");
 });
 
