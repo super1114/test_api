2ed33f4e634a402e1d6c3d13236edbc1354a5006
super1114 <super1114dev@gmail.com>
2023-03-07 14:40:39 +0900

    fix

--- api/create.js
+++ api/create.js
@@ -1,6 +1,6 @@
 const app = require("express")();
 const createRouter = require("./routes/createRouter");
 
-app.use("/api/", createRouter);
+app.use("/api/nft", createRouter);
 
 module.exports = app;
