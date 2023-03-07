9a7d0a3a0c79804c9792083a6377c5c359aa8918
super1114 <super1114dev@gmail.com>
2023-03-07 11:25:19 +0900

    fix res.header in index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,7 @@
 const app = require('express')();
 
 app.get('/api', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     res.send("Hello Truecolor");
 });
 
-module.exports = app();
+module.exports = app;
